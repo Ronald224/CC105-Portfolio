41100b985d931e04e7300da938a9454ec698303c
Ronald224 <60540181+Ronald224@users.noreply.github.com>
2020-10-10 18:41:07 +0800

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>ronald JS</h2>
+<p id="demo">Im batmann.</p>
+<button type="button" onclick='document.getElementById("demo").innerHTML = "hello papa!"'>Click Me!</button>
+</body>
+</html>
